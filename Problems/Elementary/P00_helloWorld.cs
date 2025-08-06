04f035966491ee39dc10581ba305d6b977c15512
nandan <parmar.nandan189@gmail.com>
2025-08-07 00:23:09 +0530

    Add HelloWorld program and main entry point

---
+++ Problems/Elementary/P00_helloWorld.cs
@@ -0,0 +1,12 @@
+namespace ThinkSharp.Problems
+{
+    // A simple C# program to print "Hello, World!" to the console.
+
+    public class HelloWorld
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ThinkSharp.Problems;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please Enter the Problem Number:");
+        var input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "00":
+                HelloWorld.Run();
+                break;
+            default:
+                Console.WriteLine("Problem not found.");
+                return;
+        }
+    }
+}
