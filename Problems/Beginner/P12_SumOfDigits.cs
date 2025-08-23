24c302ca36939d6a7b065f31dddc774aeae5f522
nandan <parmar.nandan189@gmail.com>
2025-08-22 20:19:19 -0500

    Sum of Digits Partitioned

--- Problems/Beginner/P12_SumOfDigits.cs
+++ Problems/Beginner/P12_SumOfDigits.cs
@@ -15,32 +15,6 @@ namespace ThinkSharp.Problems
             Console.WriteLine("     PROBLEM 12: Sum of Digits          ");
             Console.WriteLine("======================================\n");
 
-            Console.Write("Do you want to solve this problem via Recursion (Y/N): ");
-            string input = Console.ReadLine()!.ToUpper();
-            bool rec = (input == "Y");
-            if (rec)
-            {
-                Console.WriteLine("Solution with Recursion:");
-                string splittedDigits = string.Empty;
-                int sum = 0;
-                Console.Write("Enter a number: ");
-                int a = Convert.ToInt16(Console.ReadLine()!);
-                SumofDigitsViaRecursion(a, ref splittedDigits, ref sum);
-
-            Console.WriteLine($"The Digits are: {splittedDigits}");
-            Console.WriteLine($"The Sum of Digits is: {sum}");
-                
-
-            }
-            else
-            {
-                WithOutRecursion();
-            }
-        }
-
-        private static void WithOutRecursion()
-        {
-            Console.WriteLine("Solution without Recursion:");
             string splittedDigits = string.Empty;
             int sum = 0;
             Console.Write("Enter a number: ");
@@ -54,19 +28,7 @@ namespace ThinkSharp.Problems
             }
             Console.WriteLine($"The Digits are: {splittedDigits}");
             Console.WriteLine($"The Sum of Digits is: {sum}");
-        }
 
-        private static void SumofDigitsViaRecursion(int number, ref string digits, ref int sum)
-        {
-            if (number == 0)
-            {
-                return;
-            }
-
-            SumofDigitsViaRecursion(number / 10, ref digits, ref sum);
-            int lastDigit = number % 10;
-            digits += lastDigit + " ";
-            sum += lastDigit;
         }
     }
 }
